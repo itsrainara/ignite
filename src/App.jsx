8e69bcd7731bb24e5cc752f3da6a347207b843f8
Rainara <rainara.cps@gmail.com>
2022-12-13 15:58:48 -0300

    news

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { Header } from "./components/Header"
 import { Post } from "./post"
+import style from './App.module.css'
 
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
 
-    
-      <Post author="rainara"
-       content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam illum nemo aut, eius sed dicta tempore neque velit aspernatur, quas illo, veritatis id ipsum officia? Saepe vero laudantium nihil provident. aaaaaaaa"
+      <div className={style.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="rainara"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam illum nemo aut, eius sed dicta tempore neque velit aspernatur, quas illo, veritatis id ipsum officia? Saepe vero laudantium nihil provident. aaaaaaaa"
+          />
+        <Post author="paula costa"
+        content="uma nova postagem foi feita"
         />
-      <Post author="paula costa"
-      content="uma nova postagem foi feita"
-      />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,5 @@
+export function Post () {
+  return (
+    <h1>Hello world</h1>
+  )
+}
